a6c4ca668abd3fd622227ac5b51634320e3ebdb2
Apps4Canadians <apps4canadians@live.com>
2014-11-03 17:29:05 -0500

    avatar
    avatar

--- js/doNavigation.js
+++ js/doNavigation.js
@@ -1551,6 +1551,252 @@ story3 layout:
 							window.location.href = 'Data.html';
 						}						
 						break;
+						
+					case 11:
+					if (_myStoryPos == "0"){	
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 1);
+								window.location.href = 'S2.html';
+							}
+						}
+						
+						else if (_myStoryPos == "1"){	
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 2);
+								window.location.href = 'S1.html'; 
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 0);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "2"){					
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 3);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 1);
+								window.location.href = 'S2.html'; 
+							}
+						}
+						
+						else if (_myStoryPos == "3"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 4);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 2);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "4"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 5);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 3);
+								window.location.href = 'S2.html';
+							}
+						}	
+						
+						else if (_myStoryPos == "5"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 6);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 4);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "6"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 7);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 5);
+								window.location.href = 'S2.html';
+							}
+						}	
+						
+						else if (_myStoryPos == "7"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 8);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 6);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "8"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 9);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 7);
+								window.location.href = 'S2.html';
+							}
+						}	
+						
+						else if (_myStoryPos == "9"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 10);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 8);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "10"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 11);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 9);
+								window.location.href = 'S2.html';
+							}
+						}	
+						
+						else if (_myStoryPos == "11"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 12);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 10);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "12"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 13);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 11);
+								window.location.href = 'S2.html';
+							}
+						}	
+						
+						else if (_myStoryPos == "13"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 14);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 12);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "14"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 15);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 13);
+								window.location.href = 'S2.html';
+							}
+						}	
+						
+						else if (_myStoryPos == "15"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 16);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 14);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "16"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 17);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 15);
+								window.location.href = 'S2.html';
+							}
+						}	
+						
+						else if (_myStoryPos == "17"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 18);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 16);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "18"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 19);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 17);
+								window.location.href = 'S2.html';
+							}
+						}	
+						
+						else if (_myStoryPos == "19"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 20);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 18);
+								window.location.href = 'S1.html';
+							}
+						}
+						
+						else if (_myStoryPos == "20"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 21);
+								window.location.href = 'S2.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 19);
+								window.location.href = 'S2.html';
+							}
+						}	
+						
+						else if (_myStoryPos == "21"){
+							if (direction == 1){
+								localStorage.setItem("A4CSportsStoryPos", 22);
+								window.location.href = 'S1.html';
+							}
+							else{
+								localStorage.setItem("A4CSportsStoryPos", 20);
+								window.location.href = 'S1.html';
+							}
+						}
+	
+						else{						
+							window.location.href = 'Data.html';
+						}						
+						break;
+						
+						
 					
 					default:
 						$('#storyText').text("Kid Stories For Sports Dads: Navigation");
